77100c1f6420cfeaa4a32a0e08de0b00bc45b9b3
xiaoxu <xiaoxu@rongcloud.cn>
2022-07-04 18:07:20 +0800

    新增IMKit 工程

---
+++ IMKit/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  IMKit
+//
+//  Created by xiaoxu on 2022/7/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ IMKit/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  IMKit
+//
+//  Created by xiaoxu on 2022/7/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
